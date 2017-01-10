87140003476df4f22a598144ef737f06ff28ee5d
time <3480652@qq.com>
2017-01-10 18:09:27 +0800

    generator

--- demo/demo_generator.py
+++ demo/demo_generator.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 l = [x * x for x in range(10)]
@@ -14,12 +13,37 @@ for i in g:
     print(i)
 
 # 斐波那契数列
-    def fibnc(max):
-        n, a, b = 0, 0, 1
-        while n < max:
-            print(b)
-            a, b = b, a + b
-            n = n + 1
-        return 'done !'
+print('斐波那契数列')
 
-fibnc(10)
+def fibnc(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done !'
+
+for i in fibnc(10):
+    print(i)
+
+# 杨辉三角
+print('杨辉三角')
+
+def triangles():
+    L = [1]
+    while True:
+        yield L
+        L.append(0)
+        L = [L[i - 1] + L[i] for i in range(len(L))]
+n = 0
+for t in triangles():
+    print(t)
+    n = n + 1
+    if n == 2:
+        break
+
+l = [1]
+l.append(0)
+print(l)
+l = [l[i - 1] + l[i] for i in range(len(l))]
+print(l)
