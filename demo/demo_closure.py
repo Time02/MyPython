8d354dd9aeab6f81363f95e2aa5c2fc702545f9b
time <3480652@qq.com>
2017-01-12 18:00:23 +0800

    closure

--- demo/demo_closure.py
+++ demo/demo_closure.py
@@ -6,11 +6,7 @@ def count():
         def f():
              return i*i
         fs.append(f)
-        print(i)
     return fs
 
 f1, f2, f3 = count()
-print(f1())
-e = range(3)
-print(e)
-print([i for i in range(1,4)])
+print(f1)
