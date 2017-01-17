a19a177ff25bf1855a2c259135e67e539a8027ef
time <3480652@qq.com>
2017-01-17 18:07:44 +0800

    demo

---
+++ demo/demo_debug.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# def foo(s):
+#     n = int(s)
+#     print('>>> n = %d' % n)
+#     return 10 / n
+#
+# def main():
+#     foo('0')
+
+s = '0'
+n = int(s)
+print(10 / n)
